c024c65a97606644f1cc6c2443d2f98f526725b2
thestonegod <elshaddaisandstone@gmail.com>
2025-11-11 19:54:15 -0800

    Tryna remember some stuff

--- gone.py
+++ gone.py
@@ -1,3 +1,8 @@
 #Lists
 list = [ "Eye", "Nose", "stomach", 3, " Kick them where it hurts"]
-print ( list )
+#Printing by index
+print ( list [2] )
+
+#Calling out the value itself
+list = "gulp"
+print ("gulp")
